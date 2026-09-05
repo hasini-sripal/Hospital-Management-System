7e0925ca87566562ed0920716077854f78652436
hasini-sripal <hasini.sripal08@gmail.com>
2026-09-05 11:02:31 +0530

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,14 +1,17 @@
-import logging
+import logging, config_loader
 
-def get_logger():
+def get_logger() -> logging.Logger:
     logger = logging.getLogger('HospitalSystem')
     logger.setLevel(logging.INFO)
     
     if logger.handlers:
         logger.handlers.clear()
+
+    config = config_loader.load_config('config.json')
+    log_file = config.get("log_file", "hospital_system.log")
     
     formatter = logging.Formatter('%(asctime)s - %(filename)s - %(name)s - %(message)s')
-    file_handler = logging.FileHandler('hospital.log')
+    file_handler = logging.FileHandler(log_file)
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
     
